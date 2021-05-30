1872bbe0986ba87e274276cddb57340a5a72fa9c
mebangit123 <mebannongrum333@gmail.com>
2021-05-30 13:27:34 +0530

    Merging EmpWageUC1 to main branch

---
+++ EmpWage/EmpWage.js
@@ -0,0 +1,13 @@
+//UC 1 IF ELSE Condition
+{
+	const IS_ABSENT = 0;
+	let empCheck = Math.floor(Math.random() * 10) %2;
+	if(empCheck == IS_ABSENT)
+	{
+		console.log("Emloyee is ABSENT");
+	}
+	else
+	{
+		console.log("Employee is PRESENT");
+	}
+}
