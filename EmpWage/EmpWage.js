1a048ea50f8b58777bb43f6ae4eeaf828b9d76a6
mebangit123 <mebannongrum333@gmail.com>
2021-05-30 14:34:01 +0530

    Refractored to calculate wage till number of working days or total working hours per month is reached

--- EmpWage/EmpWage.js
+++ EmpWage/EmpWage.js
@@ -13,14 +13,15 @@
 	}
 }
 
-// UC 2,3,4
+// UC 2,3,4,5
 
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-const NUM_OF_WORKING_DAYS = 2;
+const NUM_OF_WORKING_DAYS = 10;
+const MAX_HRS_IN_MONTH = 100;
 
 function getWorkingHours(empCheck)
 {
@@ -34,11 +35,16 @@ function getWorkingHours(empCheck)
 				return 0;
 	}
 }
-let empHrs = 0;
-for(let day = 0; day < NUM_OF_WORKING_DAYS; day++)
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+while(totalEmpHrs <= MAX_HRS_IN_MONTH &&
+		totalWorkingDays < NUM_OF_WORKING_DAYS)
 {
+	totalWorkingDays++;
 	let empCheck = Math.floor(Math.random() * 10) % 3;
-	 empHrs += getWorkingHours(empCheck);
+	 totalEmpHrs += getWorkingHours(empCheck);
 }
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Emp Wage: " + empWage);
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("Total Days: " + totalWorkingDays +
+				"\nTotal Hrs: "+ totalEmpHrs +
+				"\nEmp Wage: "+ empWage);
