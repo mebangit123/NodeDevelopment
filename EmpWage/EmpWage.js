3a67a955419c56134269f434e5a0819439756c66
mebangit123 <mebannongrum333@gmail.com>
2021-05-30 15:04:18 +0530

    Refractored to store Employee Daily wage in a Array

--- EmpWage/EmpWage.js
+++ EmpWage/EmpWage.js
@@ -20,8 +20,13 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-const NUM_OF_WORKING_DAYS = 10;
-const MAX_HRS_IN_MONTH = 100;
+const NUM_OF_WORKING_DAYS = 2;
+const MAX_HRS_IN_MONTH = 160;
+
+function calcDailyWage(empHrs)
+{
+	return empHrs * WAGE_PER_HOUR;
+}
 
 function getWorkingHours(empCheck)
 {
@@ -37,14 +42,20 @@ function getWorkingHours(empCheck)
 }
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
+let empDailyWageArr =  new Array();
+
 while(totalEmpHrs <= MAX_HRS_IN_MONTH &&
 		totalWorkingDays < NUM_OF_WORKING_DAYS)
 {
 	totalWorkingDays++;
 	let empCheck = Math.floor(Math.random() * 10) % 3;
-	 totalEmpHrs += getWorkingHours(empCheck);
+	let empHrs = getWorkingHours(empCheck);
+	totalEmpHrs += empHrs;
+	empDailyWageArr.push(calcDailyWage(empHrs));
 }
+
 let empWage = totalEmpHrs * WAGE_PER_HOUR;
 console.log("Total Days: " + totalWorkingDays +
 				"\nTotal Hrs: "+ totalEmpHrs +
 				"\nEmp Wage: "+ empWage);
+console.log(empDailyWageArr);
