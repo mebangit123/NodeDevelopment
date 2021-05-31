722e91948a68b48be381d76d40301b149f7594e1
mebangit123 <mebannongrum333@gmail.com>
2021-05-31 14:14:26 +0530

    Merging EmpwageUC9 to main branch

--- EmpWage/EmpWage.js
+++ EmpWage/EmpWage.js
@@ -26,6 +26,7 @@ let totalEmpHrs = 0;
 let totalWorkingDays = 0;
 let empDailyWageArr =  new Array();
 let empDailyWageMap = new Map();
+let empDailyHrsMap = new Map();
 
 
 function calcDailyWage(empHrs)
@@ -54,6 +55,7 @@ while(totalEmpHrs <= MAX_HRS_IN_MONTH &&
 	let empHrs = getWorkingHours(empCheck);
 	totalEmpHrs += empHrs;
 	empDailyWageArr.push(calcDailyWage(empHrs));
+	empDailyHrsMap.set(totalWorkingDays, empHrs);
 	empDailyWageMap.set(totalWorkingDays, calcDailyWage(empHrs));
 }
 
@@ -137,3 +139,31 @@ console.log("Number of Days Emp Worked: "+
 // UC 8
 console.log("Emp Wage Map totalHrs: "+
 				Array.from(empDailyWageMap.values()).reduce(totalWages, 0));
+
+// UC 9
+const findTotal = (totalVal, dailyVal) => 
+{
+	return totalVal + dailyVal;
+}
+let totalHours = Array.from(empDailyHrsMap.values())
+						.filter(dailyHrs => dailyHrs > 0)
+						.reduce(findTotal, 0);
+let totalSalary = empDailyWageArr
+							.filter(dailyWage => dailyWage > 0)
+							.reduce(findTotal, 0);
+console.log("Emp Wage With Arrow: " + "TotalHours :" +
+					totalHours+ " Total Wages: "+ totalSalary);
+
+let nonWorkingDays = new Array();
+let partWorkingDays = new Array();
+let fullWorkingDays = new Array();
+
+empDailyHrsMap.forEach( (value, key) => 
+{
+	if(value == 8) fullWorkingDays.push(key);
+	else if(value == 4) partWorkingDays.push(key);
+	else if(value == 0) nonWorkingDays.push(key);
+});
+console.log("Full Working Days: "+fullWorkingDays);
+console.log("Part Working Days: "+partWorkingDays);
+console.log("Non Working Days: "+nonWorkingDays);
